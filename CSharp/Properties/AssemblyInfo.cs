b52ae453e27cb2cae2d5ea0cae11c9e6c37c480e
vintasoft <alex@vintasoft.com>
2024-10-16 15:55:00 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Windows;
 [assembly: AssemblyTitle("VintaSoft WPF PDF Editor Demo")]
 [assembly: AssemblyDescription("Shows how to edit PDF documents.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VintaSoft Ltd.")]
+[assembly: AssemblyCompany("VintaSoft Llc.")]
 [assembly: AssemblyProduct("VintaSoft Imaging .NET SDK")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2024")]
+[assembly: AssemblyCopyright("Copyright VintaSoft Llc. 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
